06707891c4ec4514b490a06d8cf3801f45b5a24f
testuser-pleaseignore <test_user_please_ignore@hotmail.com>
2020-01-03 15:35:15 +0700

    commit-1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<cmath>
 
+using namespace std;
+
+double cal_y(double x,double e){
+    x = (3*(pow(x,3))) + (2*e) + (pow(2,(2*x+1)))+ (sqrt((pow(x,2))+1));
+    return x;
+
+}
+int main (){
+	double x,y;
+	const double e = 2.71828;
+	cout << "Enter x= ";
+	cin >> x; 
+	cout << "Result y = " << cal_y(x,e) ;
+}
--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
-//Write function findDistance() here
+double findDistance(double u,double a, double t){
+  return u*t + 0.5*a*t*t ;
+}
+
 
 int main(){
+  float a,t,u;
 
-  //Calling findDistance() using test cases
-
+	cout << "Enter u (m/s) = " ;
+	cin >> u;
+  cout << "Enter a (m/s^2) = " ;
+	cin >> a ;
+	cout << "Enter t (s) = ";
+	cin >> t;
+  cout << "Distance = " << findDistance(u,a,t) <<endl;
+	// v = (s/t) - (0.5*a*t) ;
+	// cout << "v (m/s) = " << v << endl ;
+	// u = 2*s/t - v ;
+	// cout << "u (m/s) = " << u << endl ;
   return 0;
 }
